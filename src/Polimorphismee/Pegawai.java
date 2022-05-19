11d454c4a1e72c8a355e3b4f62ad4cc846402451
icunsuwekrek <ikhsansani226@gmail.com>
2022-05-19 08:42:25 +0700

    first commit

---
+++ src/Polimorphismee/Pegawai.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polimorphismee;
+
+/**
+ *
+ * @author ichsan sani al azmi
+ */
+public class Pegawai {
+    String nama;
+    int gaji;
+    Pegawai(){ //konstruktor 
+        
+    }
+    Pegawai (String nm){ //deklarasi konstruktor
+        this.nama=nm;
+        System.out.println("pegawai");
+    }
+    public int gaji(){
+        return 0;
+    }
+}
